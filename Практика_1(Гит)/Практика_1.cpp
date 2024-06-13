13c9068127c358fa0aaa88cd86a67f60d202fbfd
Харченко Богдан <harchenkobo2003@gmail.com>
2024-06-13 12:44:10 +0300

    Выполнил Главу 1

---
+++ Практика_1(Гит)/Практика_1.cpp
@@ -0,0 +1,19 @@
+﻿// Практика_1.cpp :
+
+#include <iostream>
+
+int main()
+{
+	int x{};
+	std::cout << "Enter an integer: ";
+	std::cin >> x;
+
+	int y{};
+	std::cout << "Enter another integer: ";
+	std::cin >> y;
+
+	std::cout << x << " + " << y << " is " << x + y << ".\n";
+	std::cout << x << " - " << y << " is " << x - y << ".\n";
+
+	return 0;
+}
