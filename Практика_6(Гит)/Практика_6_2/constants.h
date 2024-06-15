13efc9097d3d3cf859471eb79bac13ddd7be184b
Харченко Богдан <harchenkobo2003@gmail.com>
2024-06-15 08:46:18 +0300

    Выполнил Главу 6

---
+++ Практика_6(Гит)/Практика_6_2/constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+    // удалите ключевое слово inline, если C++17 не поддерживается
+    constexpr int max_class_size{ 35 };
+}
+#endif
