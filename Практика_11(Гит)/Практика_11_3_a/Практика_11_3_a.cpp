21b111bd11017d900638b77d8f207ac917d15e31
Харченко Богдан <harchenkobo2003@gmail.com>
2024-06-19 12:42:56 +0300

    Выполнил Главу 11

---
+++ Практика_11(Гит)/Практика_11_3_a/Практика_11_3_a.cpp
@@ -0,0 +1,32 @@
+﻿#include <cassert>
+#include <numeric> // для std::midpoint
+
+
+int binarySearch(const int* array, int target, int min, int max)
+{
+    assert(array); 
+
+    while (min <= max)
+    {
+        
+        int midpoint{ std::midpoint (min, max) };
+        
+
+        if (array[midpoint] > target)
+        {
+            
+            max = midpoint - 1;
+        }
+        else if (array[midpoint] < target)
+        {
+           
+            min = midpoint + 1;
+        }
+        else
+        {
+            return midpoint;
+        }
+    }
+
+    return -1;
+}
---
+++ Практика_11(Гит)/Практика_11_3_b/Практика_11_3_b.cpp
@@ -0,0 +1,29 @@
+﻿#include <cassert>
+#include <numeric> // для std::midpoint
+
+
+int binarySearch(const int* array, int target, int min, int max)
+{
+    assert(array); 
+
+    
+
+    if (min > max)
+        return -1;
+
+    int midpoint{ std::midpoint(min, max) };
+    
+
+    if (array[midpoint] > target)
+    {
+        return binarySearch(array, target, min, midpoint - 1);
+    }
+    else if (array[midpoint] < target)
+    {
+        return binarySearch(array, target, midpoint + 1, max);
+    }
+    else
+    {
+        return midpoint;
+    }
+}
